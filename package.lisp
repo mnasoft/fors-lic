e508d71173d4d36f23412647afd37f08ccafdade
U-ZORYA\namatv <mnasoft@gmail.com>
2017-03-10 16:25:25 +0200

    2017-3-10 16:25:22 KO11-118383

--- package.lisp
+++ package.lisp
@@ -3,16 +3,25 @@
 (defpackage #:fors-lic
   (:use #:cl)
   (:export channel
-	   fors-lic
-	   )
-  (:export mass-flow-rate
+	   print-object
+	   mass-flow-rate
 	   pd
-	   mfr1-by-mfr
-	   mfr2-by-mfr
+	   )
+  (:export fors-lic
+	   pd2-by-pd1
 	   pd1-by-mfr
 	   pd2-by-mfr
-   	   pd2-by-pd1
-	   )
-  )
+	   mfr1-by-mfr
+	   mfr2-by-mfr
+	   pd2-by-pd1
+	   pd1-by-mfr
+	   pd2-by-mfr
+	   mfr1-by-mfr
+	   mfr2-by-mfr
+	   mass-flow-rate))
 
 ;;;;(declaim (optimize (space 0) (compilation-speed 0)  (speed 0) (safety 3) (debug 3)))
+
+
+
+
